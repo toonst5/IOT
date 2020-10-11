202cfa68d2917fd8789a2e2610c21b8431da9d15
toonst5 <61407843+toonst5@users.noreply.github.com>
2020-10-11 14:22:33 +0200

    Add files via upload

---
+++ dataTest.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "student_11902966";
+$password = "3elw5t9f1d7h";
+$dbname = "student_11902966";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(255) NOT NULL,
+lastname VARCHAR(255) NOT NULL,
+email VARCHAR(255),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
